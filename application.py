ec35e1ebc735e2d3ac6a897db6409a050c4f5626
udayj <jhunjhunwala.uday@gmail.com>
2015-08-18 21:44:35 +0530

    initial hello world commit for code base

---
+++ application.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, render_template, Response, redirect, url_for,flash, jsonify
+SECRET_KEY='SECRET'
+
+
+SALT='123456789passwordsalt'
+
+app = Flask(__name__)
+
+@app.route('/')
+def main_page():
+	return render_template('main_page.html')
+
+if __name__ == "__main__":
+    app.run()
