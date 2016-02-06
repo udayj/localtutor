449f56dae9a9a88c851eead94da2882e1b296884
udayj <jhunjhunwala.uday@gmail.com>
2016-02-06 15:10:12 +0530

    fixing fb redirection bug from singup page

--- application.py
+++ application.py
@@ -464,7 +464,7 @@ def login():
 
 		
 		if login_user(ret_user,force=True):
-			if 'redirect_url' in data and data['redirect_url'][-6:]!='login2':
+			if 'redirect_url' in data and data['redirect_url'][-6:]!='signup':
 				return redirect(data['redirect_url'])
 			else:
 				return redirect('/user_profile?id='+ret_user.id)
